c9b61910835cc5c543b50a758be8daca59a7e261
Jinyong <kjy040216@kbu.ac.kr>
2024-10-30 09:58:21 +0900

    update

--- week9/week9/app/src/main/java/com/example/week9/MainActivity.java
+++ week9/week9/app/src/main/java/com/example/week9/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.week9;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +18,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    CheckBox checkBox;
+    LinearLayout linearLayout;
+    TextView empty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        empty = findViewById(R.id.empty);
+        linearLayout = findViewById(R.id.notempty);
+
+        checkBox = findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(checkBox.isChecked()) {
+                    linearLayout.setVisibility(View.VISIBLE);
+                    empty.setVisibility(View.INVISIBLE);
+                } else {
+                    linearLayout.setVisibility(View.INVISIBLE);
+                    empty.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
 
     }
 }
---
+++ week9/week9/app/src/main/java/com/example/week9/SampleListView.java
@@ -0,0 +1,20 @@
+package com.example.week9;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class SampleListView extends AppCompatActivity {
+}
